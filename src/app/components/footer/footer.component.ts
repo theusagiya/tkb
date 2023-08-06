a6d97fb1e8b5595f0130ed953a70703971d32cfa
theusagiya <lucamendes.developerjs@gmail.com>
2023-08-06 15:54:17 -0300

    add resposive

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  @Input() typeFooter: 'footer-one' | 'footer-two' = 'footer-one';
+
+
 
 }
